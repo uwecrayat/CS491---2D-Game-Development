6c5f997684bc706a08ceb3198a6a4de484aebeaf
uwecrayat <rayat@uwec.edu>
2015-10-06 21:50:04 -0500

    Added expanding paddle animation. Paddle graphics now separated into parts

---
+++ Arkanoid/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public int state;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animator> ().enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown(KeyCode.Space)) {
+
+		}
+	}
+
+	void ChangeState(string state) {
+		switch (state) {
+		case "large":
+			//play animation to enlarge paddle
+			break;
+		case "laser": 
+			//change sprite to laser and give laser ability
+			break;
+		}
+	}
+}
