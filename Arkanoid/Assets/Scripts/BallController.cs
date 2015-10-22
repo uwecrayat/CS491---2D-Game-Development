f96dd8cbda069e320a07aa3fc7c4850356e67d49
uwecrayat <rayat@uwec.edu>
2015-10-22 12:42:54 -0500

    Moved all animations to animation folder. Catch and expand powerups implemented

--- Arkanoid/Assets/Scripts/BallController.cs
+++ Arkanoid/Assets/Scripts/BallController.cs
@@ -7,78 +7,79 @@ public class BallController : MonoBehaviour {
     public AudioClip blockMultiSound;
     private AudioSource audioSource;
     private Rigidbody2D rb2D;
-	private bool ballInPlay;
-	private Vector2 initVel;
-	private Vector2 velocity;
-	private LineRenderer lr;
-	private int count;
+    public bool ballInPlay;
+    private Vector2 initVel;
+    private Vector2 velocity;
+    private LineRenderer lr;
+    private int count;
     private bool blockMultiHit;
-	// Use this for initialization
-	void Start () {
+    private float ballOffset;
+    // Use this for initialization
+    void Start() {
         audioSource = GetComponent<AudioSource>();
-		rb2D = GetComponent<Rigidbody2D> ();
-		lr = GetComponent<LineRenderer> ();
-		initVel = new Vector2 (1f, 3f);
-		count = 1;
-	}
-	
-	// Update is called once per frame
-	void Update() {
-		if (!ballInPlay) {
-			rb2D.velocity = new Vector2(0,0);
-			transform.position = new Vector3 (player.transform.position.x, -2.57f, 0);
-			lr.SetPosition(0, transform.position);
-		}
-	}
+        rb2D = GetComponent<Rigidbody2D>();
+        lr = GetComponent<LineRenderer>();
+        initVel = new Vector2(1f, 3f);
+        count = 1;
+        ballOffset = 0;
+    }
 
-	void FixedUpdate () {
-		velocity = rb2D.velocity;
+    // Update is called once per frame
+    void Update() {
+        if (!ballInPlay) {
+            rb2D.velocity = new Vector2(0, 0);
+            transform.position = new Vector3(player.transform.position.x - ballOffset, -2.55f, 0);
+            lr.SetPosition(0, transform.position);
+        }
+    }
 
-		if (Input.GetKeyDown (KeyCode.Space) && !ballInPlay) {
-			ballInPlay = true;
-			print("firing " + initVel);
-			rb2D.velocity = (initVel);
-		}
+    void FixedUpdate() {
+        velocity = rb2D.velocity;
 
-		// debug path drawing
+        if (Input.GetKeyDown(KeyCode.Space) && !ballInPlay) {
+            ballInPlay = true;
+            print("firing " + initVel);
+            rb2D.velocity = (initVel);
+        }
+        if (Input.GetKeyDown(KeyCode.Z) && ballInPlay) {
+            rb2D.velocity = new Vector2(rb2D.velocity.x * 0.5f, rb2D.velocity.y * 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X) && ballInPlay) {
+            rb2D.velocity = new Vector2(rb2D.velocity.x / 0.5f, rb2D.velocity.y / 0.5f);
+        }
+
+        // debug path drawing
         //if (ballInPlay) {
         //    count++;
         //    lr.SetVertexCount (count);
         //lr.SetPosition (count - 1, transform.position);
         //}
-	}
-	
+    }
 
-	void OnCollisionEnter2D(Collision2D coll) {
-        if (coll.gameObject.tag == "block")
-        {
-            if (blockMultiHit)
-            {
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.tag == "block") {
+            if (blockMultiHit) {
                 blockMultiHit = false;
                 audioSource.PlayOneShot(blockMultiSound);
-            }
-            else
-            {
+            } else {
                 audioSource.PlayOneShot(blockHit);
                 blockMultiHit = true;
             }
         }
-		string hitName = coll.gameObject.GetComponentInChildren<BoxCollider2D> ().name;
-		print (hitName);
-		gameObject.GetComponent<CircleCollider2D> ().enabled = false;
-		velocity = Vector2.Reflect (velocity, coll.contacts [0].normal);
-		print ("contacts[0] " + coll.contacts [0].point);
-		print ("paddle position " + player.transform.position);
-		print (velocity);
-		initVel = velocity;
-		rb2D.velocity = velocity;
-       
-		StartCoroutine (CollisionFix ());
-	}
+        ballOffset = player.transform.position.x - coll.contacts[0].point.x;
 
-	IEnumerator CollisionFix() {
-		yield return new WaitForSeconds (0.01f);
-		gameObject.GetComponent<CircleCollider2D> ().enabled = true;
-	}
-		
+        gameObject.GetComponent<CircleCollider2D>().enabled = false;
+        velocity = Vector2.Reflect(velocity, coll.contacts[0].normal);
+        initVel = velocity;
+        rb2D.velocity = velocity;
+        StartCoroutine(CollisionFix());
+    }
+
+    IEnumerator CollisionFix() {
+        yield return new WaitForSeconds(0.01f);
+        gameObject.GetComponent<CircleCollider2D>().enabled = true;
+    }
+
 }
--- Arkanoid/Assets/Scripts/PlayerController.cs
+++ Arkanoid/Assets/Scripts/PlayerController.cs
@@ -2,54 +2,64 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	private Animator animator;
+    private Animator animator;
     private AudioSource audioSource;
-	private Rigidbody2D rb2D;
-	public float speed;
+    private Rigidbody2D rb2D;
+    public float speed;
     public AudioClip paddleHit;
     public AudioClip paddleStick;
 
-	public int state;
-	// Use this for initialization
-	void Start () {
+    public string state;
+    // Use this for initialization
+    void Start() {
         audioSource = GetComponent<AudioSource>();
-		rb2D = GetComponent<Rigidbody2D> ();
-		animator = GetComponentInChildren<Animator> ();
-		animator.SetInteger ("State", -1);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-		float push = Input.GetAxis ("Horizontal");
-		rb2D.velocity = new Vector3 (push * speed, 0, 0);
-	if (Input.GetKeyDown(KeyCode.RightShift)) {
-			ChangeState("large");
-	}
+        rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+        animator.SetInteger("State", -1);
+    }
 
-	if (Input.GetKeyDown(KeyCode.LeftShift)) {
-			ChangeState("revert");
-		}
-	}
+    // Update is called once per frame
+    void Update() {
 
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag == "ball")
-        {
+        float push = Input.GetAxis("Horizontal");
+        rb2D.velocity = new Vector3(push * speed, 0, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+
+        string tag = coll.gameObject.tag;
+        if (tag != "ball" && tag != "Untagged") {
+            Destroy(coll.gameObject);
+        }
+        switch (tag) {
+            case "catch":
+                state = "catch";
+                break;
+            case "expand":
+                animator.SetInteger("State", 1);
+                state = "expand";
+                break;
+            case "laser":
+                state = "laser";
+                break;
+            case "slow":
+                state = "slow";
+                break;
+            case "multi":
+                state = "multi";
+                break;
+            
+        }
+        //revert size if any other powerup
+        if (animator.GetInteger("State") == 1 && tag != "expand") {
+            animator.SetInteger("State", 2);
+        }
+        if (state == "catch" && coll.gameObject.tag == "ball") {
+            //play stick sound
+            coll.gameObject.GetComponent<BallController>().ballInPlay = false;
+        } else if (coll.gameObject.tag == "ball") {
             audioSource.PlayOneShot(paddleHit);
         }
 
     }
-	void ChangeState(string state) {
-		switch (state) {
-		case "large":
-			//play animation to enlarge paddle
-			animator.SetInteger("State", 1);
-			break;
-		case "revert": 
-			//change sprite to laser and give laser ability
-			animator.SetInteger("State", 2);
-			break;
-		}
-	}
 }
